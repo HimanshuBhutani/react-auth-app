fa46de87a46932c9cf59fa5ce901f2c419c79846
Himanshu Bhutani <bhutanihimanshu99@gmail.com>
2020-04-10 14:53:35 +0530

    changes-to-structure

--- server/server.js
+++ server/server.js
@@ -1,29 +1,35 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+const loopbackConsole = require('loopback-console');
+const bodyParser = require('body-parser');
 
-var app = module.exports = loopback();
 
-app.start = function() {
+const app = module.exports = loopback();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
+  // adding loopback console => node . --console
+  if (loopbackConsole.activated()) {
+    loopbackConsole.start(app, {
+      prompt: 'React-auth-app',
+    });
+  } else if (require.main === module) {
     app.start();
+  }
 });
